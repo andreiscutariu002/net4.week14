858fcad44a316608aa1ac9f82c54c7bd3707023f
ascutariu <andreiscutariu92@gmail.com>
2019-07-23 18:39:57 +0300

    initial comit

---
+++ Users.Web/Users.Web/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Users.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
